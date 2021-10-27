bcffa504bf4a0781f5b7fb09bae0b3fd21e8f8f1
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-27 19:36:45 -0300

    Added room type and scaffolding

--- reactive-spring-front/reservation-app/src/app/app.component.ts
+++ reactive-spring-front/reservation-app/src/app/app.component.ts
@@ -14,4 +14,27 @@ export class AppComponent {
 
   private baseUrl:string = 'http//localhost:8080';
   private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
+
+  rooms: Room[] = [];
+
+  ngOnInit() {
+    this.rooms = [
+    new Room("127","127", "150"),
+    new Room("137","137", "180"),
+    new Room("254","254", "200")
+    ]
+  }
+}
+
+
+export class Room{
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber:string, price:string){
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
 }
