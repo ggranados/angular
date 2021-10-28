d03dae7399474351682c1970c9960aa3ab36fbf3
Guillermo Granados <guillermo.granados@outlook.com>
2021-10-28 17:27:55 -0300

    Added reservation service

--- reactive-spring-front/reservation-app/src/app/reservation.service.ts
+++ reactive-spring-front/reservation-app/src/app/reservation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,35 @@ export class ReservationService {
 
   private baseUrl:string = 'http//localhost:8080';
   private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
+
+  createReservation(body: ReservationRequest): Observable<Reservation>{
+    let httpOptions = {
+      headers: new HttpHeaders({'Content-Type':'application-json'})
+    }
+
+    return this.http.post<Reservation>(this.reservationUrl, body, httpOptions);
+  }
+}
+
+export class ReservationRequest {
+
+  roomNumber?: number;
+  checkIn?: string;
+  checkOut?: string;
+  price?: number;
+
+  constructor(roomNumber: number, checkIn: string, checkOut: string, price: number){
+    this.roomNumber = roomNumber;
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+    this.price = price;
+  }
+}
+
+export interface Reservation {
+  id: string;
+  roomNumber: number;
+  checkIn: Date;
+  checkOut: Date;
+  price: number;
 }
